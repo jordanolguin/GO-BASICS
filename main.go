55d02f89db8e92e4d056d8fddad82a4868dc314a
Jordan Olguin <jdolguin62@gmail.com>
2025-09-29 15:53:49 -0600

    wip for closures/functions

--- main.go
+++ main.go
@@ -1,33 +1,26 @@
 package main
 
-import (
-	"fmt"
-)
-
-type transformFn func(int) int
-
-// type anotherFn func(int, []string, map[string][]int) ([]int, string)
+import "fmt"
 
 func main() {
-	numbers := []int{1, 2, 3, 4}
-	moreNumbers := []int{5, 1, 2}
+	numbers := []int{1, 2, 3}
+
+	double := createTransformer(2)
+	triple := createTransformer(3)
+
+	transformed := transformNumbers(&numbers, func(number int) int {
+		return number * 2
+	})
+
 	doubled := transformNumbers(&numbers, double)
 	tripled := transformNumbers(&numbers, triple)
 
+	fmt.Println(transformed)
 	fmt.Println(doubled)
 	fmt.Println(tripled)
-
-	transformerFn1 := getTransformerFunction(&numbers)
-	transformerFn2 := getTransformerFunction(&moreNumbers)
-
-	transformedNumbers := transformNumbers(&numbers, transformerFn1)
-	moreTransformedNumbers := transformNumbers(&moreNumbers, transformerFn2)
-
-	fmt.Println(transformedNumbers)
-	fmt.Println(moreTransformedNumbers)
 }
 
-func transformNumbers(numbers *[]int, transform transformFn) []int {
+func transformNumbers(numbers *[]int, transform func(int) int) []int {
 	dNumbers := []int{}
 
 	for _, val := range *numbers {
@@ -37,18 +30,8 @@ func transformNumbers(numbers *[]int, transform transformFn) []int {
 	return dNumbers
 }
 
-func getTransformerFunction(numbers *[]int) transformFn {
-	if (*numbers)[0] == 1 {
-		return double
-	} else {
-		return triple
+func createTransformer(factor int) func(int) int {
+	return func(number int) int {
+		return number * factor
 	}
-}
-
-func double(number int) int {
-	return number * 2
-}
-
-func triple(number int) int {
-	return number * 3
 }
